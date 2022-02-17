a3cbcb8a7dd86c867acd9f798a2df13cca527255
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-17 12:25:03 +0600

    save button validation added

--- js/app.js
+++ js/app.js
@@ -7,18 +7,22 @@ document.getElementById('caluculate-btn').addEventListener('click', function () 
     const totalExpenseDisplay = getElement('total-expense');
     const remainBalanceDisplay = getElement('remain-balance');
 
+    //total expenses calculation
     const totalExpense = parseFloat(foodExpense.value) + parseFloat(rentExpense.value) + parseFloat(colthExpense.value);
+
     totalExpenseDisplay.innerText = totalExpense;
     const remaingBalance = caluculateRemainingBalance(incomeInput,totalExpense);
     remainBalanceDisplay.innerText = remaingBalance;
 
 });
 
+//get element function
 function getElement(id) {
     const input = document.getElementById(id);
     return input;
 }
 
+//calculating remaining balance
 function caluculateRemainingBalance(amount,totalUsed) {
     const remainingBalance = parseFloat(amount) - totalUsed;
     return remainingBalance;
@@ -31,12 +35,21 @@ document.getElementById('save-button').addEventListener('click',function(){
     const finalBalanceDisplay = getElement('remaining-balance');
     const incomeInput = getElement('income-input').value;
 
+    //calculating saved balance
     const savedAmount = parseFloat(incomeInput)* (parseFloat(saveInput)/100);
-    saveAmounDisplay.innerText = savedAmount;
-
+    
     const remainBalanceDisplayValue = getElement('remain-balance').innerText;
-    const finalBalance = caluculateRemainingBalance(remainBalanceDisplayValue,savedAmount);
-
-    finalBalanceDisplay.innerText = finalBalance;
+    if(savedAmount>remainBalanceDisplayValue){
+        alert('You have not enough balance to save that you want');
+    }
+    else if(savedAmount<0){
+        alert('Please enter correct value');
+    }
+    else{
+        const finalBalance = caluculateRemainingBalance(remainBalanceDisplayValue,savedAmount);
+        saveAmounDisplay.innerText = savedAmount;
+        finalBalanceDisplay.innerText = finalBalance;
+    }
+    
 })
 
