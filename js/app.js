951cb304216eabcbfbd1d99d1f048249ab559dd9
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-17 11:42:11 +0600

    save button event added

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
-document.getElementById('caluculate-btn').addEventListener('click',function(){
-    const foodExpense = getInput('food-expense');
-    const rentExpense = getInput('rent-expense');
-    const colthExpense = getInput('cloth-expense');
-    const totalExpenseDisplay = getInput('total-expense');
-    const remainBalanceDisplay = getInput('remain-balance');
+//handle calculate button
+document.getElementById('caluculate-btn').addEventListener('click', function () {
+    const incomeInput = getElement('income-input').value;
+    const foodExpense = getElement('food-expense');
+    const rentExpense = getElement('rent-expense');
+    const colthExpense = getElement('cloth-expense');
+    const totalExpenseDisplay = getElement('total-expense');
+    const remainBalanceDisplay = getElement('remain-balance');
 
-    const totalExpense =parseFloat(foodExpense.value)+parseFloat(rentExpense.value)+parseFloat(colthExpense.value);
+    const totalExpense = parseFloat(foodExpense.value) + parseFloat(rentExpense.value) + parseFloat(colthExpense.value);
     totalExpenseDisplay.innerText = totalExpense;
-    const remaingBalance = caluculateRemainingBalance(totalExpense);
+    const remaingBalance = caluculateRemainingBalance(incomeInput,totalExpense);
     remainBalanceDisplay.innerText = remaingBalance;
 
 });
 
-function getInput(id){
+function getElement(id) {
     const input = document.getElementById(id);
     return input;
 }
 
-function caluculateRemainingBalance(totalUsed){
-    const incomeInput = getInput('income-input').value;
-    const remainingBalance = parseFloat(incomeInput) - totalUsed;
+function caluculateRemainingBalance(amount,totalUsed) {
+    const remainingBalance = parseFloat(amount) - totalUsed;
     return remainingBalance;
 }
+
+//handle save button
+document.getElementById('save-button').addEventListener('click',function(){
+    const saveInput = getElement('save-percentage').value;
+    const saveAmounDisplay = getElement('saved-amount');
+    const finalBalanceDisplay = getElement('remaining-balance');
+    const incomeInput = getElement('income-input').value;
+
+    const savedAmount = parseFloat(incomeInput)* (parseFloat(saveInput)/100);
+    saveAmounDisplay.innerText = savedAmount;
+
+    const remainBalanceDisplayValue = getElement('remain-balance').innerText;
+    const finalBalance = caluculateRemainingBalance(remainBalanceDisplayValue,savedAmount);
+
+    finalBalanceDisplay.innerText = finalBalance;
+})
+
