bbd1cb234647e00941515bc62e58490887ef2fd4
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-17 14:25:20 +0600

    calculate button validation added

--- js/app.js
+++ js/app.js
@@ -7,49 +7,56 @@ document.getElementById('caluculate-btn').addEventListener('click', function () 
     const totalExpenseDisplay = getElement('total-expense');
     const remainBalanceDisplay = getElement('remain-balance');
 
-    //total expenses calculation
-    const totalExpense = parseFloat(foodExpense.value) + parseFloat(rentExpense.value) + parseFloat(colthExpense.value);
-
-    totalExpenseDisplay.innerText = totalExpense;
-    const remaingBalance = caluculateRemainingBalance(incomeInput,totalExpense);
-    remainBalanceDisplay.innerText = remaingBalance;
-
+    if (incomeInput > 0 && foodExpense.value > 0 && rentExpense.value > 0 && colthExpense.value > 0) {
+        //total expenses calculation
+        const totalExpense = parseFloat(foodExpense.value) + parseFloat(rentExpense.value) + parseFloat(colthExpense.value);
+        if (totalExpense > incomeInput) {
+            alert('Your expenses is more than your income');
+            return;
+        }
+        totalExpenseDisplay.innerText = totalExpense;
+        const remaingBalance = caluculateRemainingBalance(incomeInput, totalExpense);
+        remainBalanceDisplay.innerText = remaingBalance;
+    }
+    else {
+        alert('Please input correct amount');
+    }
 });
 
 //get element function
 function getElement(id) {
     const input = document.getElementById(id);
     return input;
-}
+};
 
 //calculating remaining balance
-function caluculateRemainingBalance(amount,totalUsed) {
+function caluculateRemainingBalance(amount, totalUsed) {
     const remainingBalance = parseFloat(amount) - totalUsed;
     return remainingBalance;
-}
+};
 
 //handle save button
-document.getElementById('save-button').addEventListener('click',function(){
+document.getElementById('save-button').addEventListener('click', function () {
     const saveInput = getElement('save-percentage').value;
     const saveAmounDisplay = getElement('saved-amount');
     const finalBalanceDisplay = getElement('remaining-balance');
     const incomeInput = getElement('income-input').value;
 
     //calculating saved balance
-    const savedAmount = parseFloat(incomeInput)* (parseFloat(saveInput)/100);
-    
+    const savedAmount = parseFloat(incomeInput) * (parseFloat(saveInput) / 100);
+
     const remainBalanceDisplayValue = getElement('remain-balance').innerText;
-    if(savedAmount>remainBalanceDisplayValue){
-        alert('You have not enough balance to save that you want');
+    if (savedAmount > remainBalanceDisplayValue) {
+        alert('You have not enough balance that you want to save');
     }
-    else if(savedAmount<0){
+    else if (savedAmount < 0) {
         alert('Please enter correct value');
     }
-    else{
-        const finalBalance = caluculateRemainingBalance(remainBalanceDisplayValue,savedAmount);
+    else {
+        const finalBalance = caluculateRemainingBalance(remainBalanceDisplayValue, savedAmount);
         saveAmounDisplay.innerText = savedAmount;
         finalBalanceDisplay.innerText = finalBalance;
     }
-    
-})
+
+});
 
