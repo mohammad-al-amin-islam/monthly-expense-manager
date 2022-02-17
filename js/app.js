26fed077de9f6780927ed93689f771019c4c190b
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-17 17:48:03 +0600

    some error fixed and validation added

--- js/app.js
+++ js/app.js
@@ -1,26 +1,37 @@
 //handle calculate button
 document.getElementById('caluculate-btn').addEventListener('click', function () {
-    const incomeInput = getElement('income-input').value;
+    const incomeInput = getElement('income-input');
     const foodExpense = getElement('food-expense');
     const rentExpense = getElement('rent-expense');
     const colthExpense = getElement('cloth-expense');
     const totalExpenseDisplay = getElement('total-expense');
     const remainBalanceDisplay = getElement('remain-balance');
 
-    if (incomeInput > 0 && foodExpense.value > 0 && rentExpense.value > 0 && colthExpense.value > 0) {
-        //total expenses calculation
-        const totalExpense = parseFloat(foodExpense.value) + parseFloat(rentExpense.value) + parseFloat(colthExpense.value);
-        if (totalExpense > incomeInput) {
-            alert('Your expenses is more than your income');
-            return;
-        }
-        totalExpenseDisplay.innerText = totalExpense;
-        const remaingBalance = caluculateRemainingBalance(incomeInput, totalExpense);
-        remainBalanceDisplay.innerText = remaingBalance;
+    const foodExpenseValue = parseFloat(foodExpense.value);
+    const rentExpenseValue = parseFloat(rentExpense.value);
+    const colthExpenseValue = parseFloat(colthExpense.value);
+    const incomeInputValue = parseFloat(incomeInput.value);
+
+    //error handling when not a number;
+    if (isNaN(foodExpenseValue) || isNaN(rentExpenseValue) || isNaN(colthExpenseValue) || isNaN(incomeInputValue)) {
+        alert('You can not write anything here without Number');
+        return;
     }
-    else {
-        alert('Please input correct amount');
+    //error handling when number is negative;
+    else if (incomeInputValue < 0 || foodExpenseValue < 0 || rentExpenseValue < 0 || colthExpenseValue < 0) {
+        alert('You cannot input Negative value');
+        return;
     }
+    //total expenses calculation
+    const totalExpense = foodExpenseValue + rentExpenseValue + colthExpenseValue;
+    //validation if totalexpenses exceed income
+    if (totalExpense > incomeInputValue) {
+        alert('Your expenses is more than your Income');
+        return;
+    }
+    totalExpenseDisplay.innerText = totalExpense;
+    const remaingBalance = caluculateRemainingBalance(incomeInputValue, totalExpense);
+    remainBalanceDisplay.innerText = remaingBalance;
 });
 
 //get element function
@@ -44,19 +55,20 @@ document.getElementById('save-button').addEventListener('click', function () {
 
     //calculating saved balance
     const savedAmount = parseFloat(incomeInput) * (parseFloat(saveInput) / 100);
-
     const remainBalanceDisplayValue = getElement('remain-balance').innerText;
+
+    //error handling when save amount crossed remain balance
     if (savedAmount > remainBalanceDisplayValue) {
         alert('You have not enough balance that you want to save');
     }
+    //error handling if input will negative
     else if (savedAmount < 0) {
-        alert('Please enter correct value');
+        alert('Your savings input cannot ne Negative');
     }
     else {
         const finalBalance = caluculateRemainingBalance(remainBalanceDisplayValue, savedAmount);
         saveAmounDisplay.innerText = savedAmount;
         finalBalanceDisplay.innerText = finalBalance;
     }
-
 });
 
